b54f9738e55730ec868e6840f2736610aa972770
Stefan <stefan@neuland.io>
2015-11-09 18:33:19 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='gmi.django.profile',
+      version=version,
+      description="Manage crew profiles.",
+      long_description="""\
+XXX long description""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='django profile',
+      author='Stefan Walluhn',
+      author_email='stefan@neuland.io',
+      url='',
+      license='GPLv3',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['gmi', 'gmi.django'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
